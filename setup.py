31a7769a837bb5d40cbcb6f4849971807b132725
Glenn Siegman <gsiegman@Glenn-Siegmans-MacBook-Pro.local>
2010-03-24 19:55:37 -0500

    added setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+#This project forked from http://github.com/tstone/django-uploadify
+#which lacked a setup.py file.
+
+from distutils.core import setup
+
+setup(name='django-uploadify',
+        description='Django integration of Uploadify jQuery plugin.',
+        author='Glenn Siegman',
+        author_email='gsiegman@gsiegman.com',
+        url='http://github.com/gsiegman/django-uploadify',
+        packages=['uploadify']
+)
