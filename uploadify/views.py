d76ff6d3356ab493d20214ba3774c812c4ac33dd
Frol <frol@ewadev.com>
2010-12-20 23:44:45 +0200

    fix for django 1.2.3

--- uploadify/views.py
+++ uploadify/views.py
@@ -1,12 +1,14 @@
+from django.dispatch import Signal
 from django.http import HttpResponse
-import django.dispatch
+from django.views.decorators.csrf import csrf_exempt
 
-upload_recieved = django.dispatch.Signal(providing_args=['data'])
+upload_received = Signal(providing_args=['data'])
 
+@csrf_exempt
 def upload(request, *args, **kwargs):
     if request.method == 'POST':
         if request.FILES:
-            upload_recieved.send(sender='uploadify', data=request.FILES['Filedata'])
-    return HttpResponse('True')
+            upload_received.send(sender='uploadify', data=request.FILES['Filedata'])
+    return HttpResponse('1')
 
 
