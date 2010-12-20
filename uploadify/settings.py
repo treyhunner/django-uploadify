fcb41feb885346ca736264267cc00aa14ee36130
Frol <frol@ewadev.com>
2010-12-20 22:09:45 +0200

    fix typo

--- uploadify/settings.py
+++ uploadify/settings.py
@@ -3,8 +3,8 @@ MEDIA_URL = getattr(settings, 'MEDIA_URL', '')
 
 # Uploadify root folder path, relative to MEDIA_ROOT
 UPLOADIFY_PATH = getattr(settings, 'UPLOADIFY_PATH', 
-    '%s%s' % (MEDIA_URL, 'js/uploadify/')
+    '%s%s' % (MEDIA_URL, 'js/uploadify/'))
 
 # Upload path that files are sent to
 UPLOADIFY_UPLOAD_PATH = getattr(settings, 'UPLOADIFY_UPLOAD_PATH', 
-    '%s%s' % (MEDIA_URL, 'uploads/')
+    '%s%s' % (MEDIA_URL, 'uploads/'))
