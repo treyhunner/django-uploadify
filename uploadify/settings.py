e53cfab28f65642f38404f5d11920f044b5340cf
Anders Petersson <me@anderspetersson.se>
2011-01-10 14:26:17 +0800

    Changed default PATHS to reflect the changes regarding STATIC_URL / MEDIA_URL in Django 1.3.

--- uploadify/settings.py
+++ uploadify/settings.py
@@ -1,9 +1,10 @@
 from django.conf import settings
+STATIC_UR:L = getattr(settings, 'STATIC_URL', '')
 MEDIA_URL = getattr(settings, 'MEDIA_URL', '')
 
-# Uploadify root folder path, relative to MEDIA_ROOT
+# Uploadify root folder path, relative to MEDIA_
 UPLOADIFY_PATH = getattr(settings, 'UPLOADIFY_PATH', 
-    '%s%s' % (MEDIA_URL, 'js/uploadify/'))
+    '%s%s' % (STATIC_URL, 'js/uploadify/'))
 
 # Upload path that files are sent to
 UPLOADIFY_UPLOAD_PATH = getattr(settings, 'UPLOADIFY_UPLOAD_PATH', 
