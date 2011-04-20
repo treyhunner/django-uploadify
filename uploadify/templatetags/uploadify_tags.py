39cc4e493b6e9b987210388d134c04babe36b681
Maxim Sukharev <max@smacker.ru>
2011-04-20 18:11:18 +0600

    add diferent senders, return data from receivers, remove auto upload

--- uploadify/templatetags/uploadify_tags.py
+++ uploadify/templatetags/uploadify_tags.py
@@ -1,5 +1,6 @@
 from django import template
 from uploadify import settings
+from django.utils import simplejson
 
 register = template.Library()
 
@@ -7,16 +8,23 @@ register = template.Library()
 #   multi_file_upload
 # -----------------------------------------------------------------------------
 @register.inclusion_tag('uploadify/multi_file_upload.html', takes_context=True)
-def multi_file_upload(context, unique_id=None):
+def multi_file_upload(context, sender=None, unique_id=None):
     """
     Displays a Flash-based interface for uploading multiple files.
     For each POST request (after file upload) send GET query with `unique_id`.
     """
+    data = {}
+
+    if sender is not None:
+        data['sender'] = str(sender)
+
     if unique_id is not None:
-        GET_query = '?unique_id=' + str(unique_id)
+        unique_id = "?unique_id=%s" % str(unique_id)
     else:
-        GET_query = ''
+        unique_id = ""
+
     return {
-        'GET_query' : GET_query,
+        'uploadify_query' : unique_id,
+        'uploadify_data' : simplejson.dumps(data),
         'uploadify_path' : settings.UPLOADIFY_PATH,
     }
--- uploadify/views.py
+++ uploadify/views.py
@@ -1,14 +1,20 @@
 from django.dispatch import Signal
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+from sugar.views.json import JsonResponse
 
 upload_received = Signal(providing_args=['request', 'data'])
 
 @csrf_exempt
 def upload(request, *args, **kwargs):
     if request.method == 'POST':
+        sender='uploadify'
+        if request.POST.has_key('sender'):
+            sender = request.POST['sender']
         if request.FILES:
-            file_url = upload_received.send(sender='uploadify', request=request,
-                data=request.FILES['Filedata'])[-1][1]
-            return HttpResponse(file_url)
+            receiveds = upload_received.send(sender=sender, request=request,
+                data=request.FILES['file'])
+            for received, response in receiveds:
+                if not response is None:
+                    return JsonResponse(response)
     return HttpResponse('1')
